b28f6117f994a3ff08881d89655f6dd2fe896116
hamza elaasemy <hamza.elaasemy@imedia24.de>
2020-06-21 23:53:51 +0100

    adding routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const mainRouter = require('./routes/main');
+const usersRouter = require('./routes/users');
 
 require('dotenv').config();
 
@@ -10,6 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//mangoDb
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
 .then(() => console.log( 'Database Connected' ))
@@ -18,9 +21,11 @@ mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedT
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log('MongoDB connected');
-
 });
-console.log(uri);
+
+//routes
+app.use('/main', mainRouter);
+app.use('/users', usersRouter);
 
 
 app.listen(port, () => {
